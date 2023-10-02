ab835e0343a6f83dcc4598e8c54e93def6795aaa
David <davidroussel171113@outlook.fr>
2023-10-02 22:53:10 +0200

    Ajout et utilisation de la fonction closeForm()

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeSpan = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+//Fermer le formulaire
+closeSpan.addEventListener("click", closeForm);
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+//Fonction pour fermer le formulaire
+function closeForm() {
+  modalbg.style.display = "none";
 }
 
 
