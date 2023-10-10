af1e50719740b63b46b7eccd234ba20f51016d28
David <davidroussel171113@outlook.fr>
2023-10-10 23:28:57 +0200

    Ajout fonctions et variables

--- JS/modal.js
+++ JS/modal.js
@@ -10,8 +10,13 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 const closeSpan = document.querySelector(".close");
+let inputNom = document.getElementById("last").value;
+let inputPrenom = document.getElementById("first").value;
+console.log(inputPrenom);
+let inputEmail = document.getElementById("email").value;
+let inputAnniversaire = document.getElementById("birthdate").value;
+let buttonSubmit = document.getElementById("submitButton");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -27,6 +32,58 @@ function launchModal() {
 //Fonction pour fermer le formulaire
 function closeForm() {
   modalbg.style.display = "none";
+}
+
+//Fonction permettant de valider les deux caractères
+function validerCaractere(champ) {
+  if (champ.length < 2) {
+    throw new Error("Le champ doit contenir au moins 2 caractères");
+  }
+}
+
+//Fonction valider l'email
+function validerEmail(champ) {
+  let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
+  if (!emailRegExp.test(champ)) {
+    throw new Error("l'e-mail n'est pas valide");
+  }
+}
+
+//Fonction pour afficher les erreurs formulaire
+function afficherMessageErreur(message) {
+  let spanError = document.getElementById("errorMessage");
+  if (!spanError) {
+    let formData = document.querySelector(".formData");
+    spanError = document.createElement("span");
+    spanError.id = "errorMessage";
+    formData.appendChild(spanError);
+  }
+  spanError.innerText = message;
+}
+
+//Fonction de gestion du formulaire
+function gererFormulaire() {
+  try {
+    validerCaractere(inputPrenom);
+    validerCaractere(inputNom);
+    validerEmail(inputEmail);
+    afficherMessageErreur("");
+  } catch (error) {
+    afficherMessageErreur(error.message);
+  }
+}
+
+//Récupération du formulaire
+let formulaire = document.querySelector("form");
+
+formulaire.addEventListener("submit", (e) => {
+  console.log(inputPrenom);
+  e.preventDefault();
+  gererFormulaire();
+});
+
+function validate() {
+  gererFormulaire();
 }
 
 
