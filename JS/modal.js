ebd4bb2c0062cf93458907d58426e7c0d6e86e24
David <davidroussel171113@outlook.fr>
2023-10-15 17:37:45 +0200

    Ajout fonction et test reception de donnée

--- JS/modal.js
+++ JS/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var myTopNav = document.getElementById("myTopnav");
+  let myTopNav = document.getElementById("myTopnav");
   if (myTopNav.className === "topnav") {
     myTopNav.className += " responsive";
   } else {
@@ -11,12 +11,17 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const closeSpan = document.querySelector(".close");
-let inputNom = document.getElementById("last").value;
-let inputPrenom = document.getElementById("first").value;
-console.log(inputPrenom);
-let inputEmail = document.getElementById("email").value;
-let inputAnniversaire = document.getElementById("birthdate").value;
+let inputNom = document.getElementById("last");
+let inputPrenom = document.getElementById("first");
+let inputEmail = document.getElementById("email");
+let inputAnniversaire = document.getElementById("birthdate");
 let buttonSubmit = document.getElementById("submitButton");
+console.log(inputAnniversaire.value);
+
+function consoleLog (value) {
+  console.log(value);
+}
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -34,9 +39,19 @@ function closeForm() {
   modalbg.style.display = "none";
 }
 
+//Fonction pour vérifié si un champ est vide
+function verifierChamp(champ) {
+  if (champ.value === "") {
+    throw new Error("Veuillez renseigner ce champ")
+  } else {
+    console.log("Ce champ est rempli")
+  }
+}
+
 //Fonction permettant de valider les deux caractères
 function validerCaractere(champ) {
   if (champ.length < 2) {
+    console.log("2 carctère putain");
     throw new Error("Le champ doit contenir au moins 2 caractères");
   }
 }
@@ -45,6 +60,7 @@ function validerCaractere(champ) {
 function validerEmail(champ) {
   let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
   if (!emailRegExp.test(champ)) {
+    console.log("email de merde")
     throw new Error("l'e-mail n'est pas valide");
   }
 }
@@ -53,37 +69,39 @@ function validerEmail(champ) {
 function afficherMessageErreur(message) {
   let spanError = document.getElementById("errorMessage");
   if (!spanError) {
-    let formData = document.querySelector(".formData");
+    let modalBody = document.querySelector(".modal-body");
     spanError = document.createElement("span");
     spanError.id = "errorMessage";
-    formData.appendChild(spanError);
+    modalBody.appendChild(spanError);
   }
   spanError.innerText = message;
 }
 
 //Fonction de gestion du formulaire
 function gererFormulaire() {
-  try {
-    validerCaractere(inputPrenom);
-    validerCaractere(inputNom);
-    validerEmail(inputEmail);
-    afficherMessageErreur("");
-  } catch (error) {
-    afficherMessageErreur(error.message);
-  }
+  buttonSubmit.addEventListener("click", function () {
+    try {
+      validerCaractere(inputPrenom.value);
+      validerCaractere(inputNom.value);
+      validerEmail(inputEmail.value);
+      verifierChamp(inputAnniversaire.value);
+      console.log(inputAnniversaire.value);
+      afficherMessageErreur("");
+    } catch (error) {
+      afficherMessageErreur(error.message);
+    }
+  })
 }
 
-//Récupération du formulaire
-let formulaire = document.querySelector("form");
-
-formulaire.addEventListener("submit", (e) => {
-  console.log(inputPrenom);
-  e.preventDefault();
-  gererFormulaire();
-});
-
 function validate() {
-  gererFormulaire();
+  //Récupération du formulaire
+  let formulaire = document.querySelector("form");
+  consoleLog(formulaire);
+
+  formulaire.addEventListener("submit", (e) => {
+    e.preventDefault();
+    gererFormulaire();
+  });
 }
 
 
